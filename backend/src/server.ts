126fd52ed2eb4668f2f11c356e0e216a0668ad0a
minhquang2k4 <minhwang1708@gmail.com>
2025-06-11 21:42:13 +0700

    fix connect db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,9 @@
 import app from "./app";
 import config from "./config/config";
-import mongoose from "mongoose";
+import { connectMongo } from "./database/mongo";
 
-mongoose
-  .connect(config.mongoUri)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-    process.exit(1);
-  });
+connectMongo();
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
